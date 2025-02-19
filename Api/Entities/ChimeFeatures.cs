b99169572c6054868a6e6e1fdbe23a5bd2f6ec5d
Michael McKechney <michael@mckechney.com>
2025-02-19 15:20:38 -0500

    Adding a version copy of the KoenZomers.Ring.Api library. There seems to be a difference between some of the output folks get than from what I get back.

---
+++ Api/Entities/ChimeFeatures.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KoenZomers.Ring.Api.Entities
+{
+    public class ChimeFeatures
+    {
+        [JsonPropertyName("ringtones_enabled")]
+        public bool RingtonesEnabled { get; set; }
+    }
+}
---
+++ Api/Entities/DeviceStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KoenZomers.Ring.Api.Entities
+{
+    public class DeviceStatus
+    {
+        /// <summary>
+        /// Contains the status of a Ring device
+        /// </summary>
+        public partial class Status
+        {
+            [JsonPropertyName("seconds_remaining")]
+            public long? SecondsRemaining { get; set; }
+        }
+    }
+}
