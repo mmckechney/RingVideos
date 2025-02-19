cc08de1025bc1de1b3c6c487fd0b700f4ee9e0a1
Michael McKechney <michael@mckechney.com>
2025-02-19 15:19:24 -0500

    To try to avoid 429 too many request errors, getting recordings by date and device ID (vs. just date) and that seems to be helping. Adding some additional helpful output and the ability to view the last 100 lines from the log file.

--- RingVideos/CommandHelper.cs
+++ RingVideos/CommandHelper.cs
@@ -26,8 +26,9 @@ namespace RingVideos
          var userNameOption = new Option<string>(new string[] { "--username", "-u" }, () => string.Empty, "Ring account username");
          var starredOption = new Option<bool>(new string[] { "--starred" }, () => false, "Flag to only download Starred videos");
          var maxcountOption = new Option<int>(new string[] { "--maxcount", "-m" }, () => 1000, "Maximum number of videos to download");
-         var debugLogOption = new Option<bool>(new string[] { "-d", "--debug" }, "Debug log option flag");
-         var traceLogOption = new Option<bool>(new string[] { "-t", "--trace" }, "Trace log option flag");
+         //var debugLogOption = new Option<bool>(new string[] { "-d", "--debug" }, "Debug log option flag");
+         //var traceLogOption = new Option<bool>(new string[] { "-t", "--trace" }, "Trace log option flag");
+         var exitAppOption = new Option<bool>(new string[] { "-x", "--exit" }, "Option to close app after running command (vs. keeping open). This could be useful in using in automated scripting. ");
          ;
          RootCommand rootCommand = new RootCommand(description: "Simple command line tool to download videos from your Ring account");
 
@@ -40,11 +41,22 @@ namespace RingVideos
          var snapshotCommand = new Command("snapshot", "Download only snapshot images");
          snapshotCommand.Handler = CommandHandler.Create<string, string, string, DateTime, DateTime>(Worker.GetSnapshotImages);
 
+         var showLogCommand = new Command("showlog", "Open the log file");
+         showLogCommand.Handler = CommandHandler.Create(Worker.ShowLog);
+
+         var quitCommand = new Command("quit", "Quit/close the application");
+         quitCommand.AddAlias("exit");
+         quitCommand.AddAlias("q");
+         quitCommand.Handler = CommandHandler.Create(Worker.QuitApplication);
+
          rootCommand.Add(starCommand);
          rootCommand.Add(allCommand);
          rootCommand.Add(snapshotCommand);
-         rootCommand.AddOption(debugLogOption);
-         rootCommand.AddOption(traceLogOption);
+         rootCommand.Add(showLogCommand);
+         rootCommand.Add(exitAppOption);
+         rootCommand.Add(quitCommand);
+         //rootCommand.AddOption(debugLogOption);
+         //rootCommand.AddOption(traceLogOption);
 
          starCommand.Add(userNameOption);
          starCommand.Add(passwordOption);
